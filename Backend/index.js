93ab11429815a07fefbb41322abff7c5546c9b4f
Harsh Suryavanshi <Harsh-Dev1602@users.noreply.github.com>
2025-08-29 13:34:41 +0530

    Deploy backend

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ import cors from "cors";
 dotenv.config();
 const app = express()
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "https://blood-donor-steel.vercel.app"}));
 
 const PORT = process.env.PORT || 3001;
 const URI = process.env.MONGODB_URI;
